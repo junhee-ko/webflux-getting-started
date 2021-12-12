e4419b4255c421039d5bd76b52f58201acd87c44
junhee-ko <junheee.ko@gmail.com>
2021-12-12 12:22:45 +0900

    first commit

---
+++ src/test/kotlin/jko/webfluxgettingstarted/WebfluxGettingStartedApplicationTests.kt
@@ -0,0 +1,13 @@
+package jko.webfluxgettingstarted
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxGettingStartedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
